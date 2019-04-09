407db4f5df724632c15d41ee356d88ed976675ff
Daniel Shroff <danielshroff@hotmail.com>
2019-04-09 16:15:53 +0530

    Add read functions

--- src/SCAPP_Client_Lib/Class1.cs
+++ src/SCAPP_Client_Lib/Class1.cs
@@ -1,8 +1,27 @@
 ﻿using System;
-
+using Base_Data_Classes;
+using System.IO;
+using System.IO.Compression;
+using System.Collections.Generic;
 namespace SCAPP_Client_Lib
 {
-    public class Class1
+    public class High
     {
+        public List<Base_Data_Classes.ConfigFileItem> DecodeConfigFile(string filePath)
+        {
+            
+        }
+
+    }
+    public class Low
+    {
+        public string ReadConfigFile(string filePath)
+        {
+            return File.ReadAllText(filePath);
+        }
+        public void WriteConfigFile()
+        {
+
+        }
     }
 }

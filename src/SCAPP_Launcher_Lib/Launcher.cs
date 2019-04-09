1ef9224b327e4805db273bd0ec0320e39e6dcf61
Daniel Shroff <danielshroff@hotmail.com>
2019-04-09 15:15:21 +0530

    Added two methods to execute the default script
    One caches the workdir, useful for multi-executable programs, one clears it, useful for preserving disk space

--- src/SCAPP_Launcher_Lib/Launcher.cs
+++ src/SCAPP_Launcher_Lib/Launcher.cs
@@ -1,37 +1,77 @@
-﻿using System;
-using System.IO.Compression;
-using System.IO;
-
+﻿using System;//YOu should know
+using System.IO.Compression;//for extracting SCAPP files
+using System.IO;//for reading files to memory
+using System.Diagnostics;//for launching stuff
 namespace SCAPP_Launcher_Lib
 {
     public class High //High level ops, facade class for easy normal usage, uses Low class
     {
-        public void ExtractToWorkFolder(string File)
+        private Low low = new Low();//instance of class Low
+        public void ExtractToWorkFolder(string file)//auto extracts a file to a working folder
         {
-            var low = new Low();
-            var dirName = File + "-workdir";
-            low.ExtractToDir(File, dirName);
+           
+            var dirName = file + "-workdir";//name of workdir
+            low.ExtractToDir(file, dirName);//call method from class Low
         }
+        public void ExecAndCache(string FilePath)
+        {
+            ExtractToWorkFolder(FilePath);
+            var dir = FilePath + "-workdir";
 
+            var scriptPath = dir + "/default.scas";
+
+            var execFiles =
+            low.DecodeScriptExecs(scriptPath);
+
+            foreach(var executable in execFiles)
+            {
+                low.ExecuteFile(executable);
+            }
+        }
+        public void ExecAndClear(string filePath)
+        {
+            ExtractToWorkFolder(filePath);
+            var dir = filePath + "-workdir";
+
+            var scriptPath = dir + "/default.scas";
+
+            var execFiles =
+            low.DecodeScriptExecs(scriptPath);
+
+            foreach (var executable in execFiles)
+            {
+                low.ExecuteFile(executable);
+            }
+            System.Threading.Thread.Sleep(10000);
+            Directory.Delete(dir, true);
+        }
     }
     public class Low //bare metal, use only if required, used by High
     {
-        public void ExtractToDir(string filePath, string dirPath)
+        public void ExecuteFile(string filePath)//reads filepath and executes, a wrapper for Process.Start() 
         {
-            ZipFile.ExtractToDirectory(filePath, dirPath);
+            Process.Start(filePath);
         }
-        public String[] DecodeScriptExecs(string scriptPath)
+
+        public void ExtractToDir(string filePath, string dirPath)//Extracting a file, simply a wrapper for ZipFile method
         {
-            var dataToReturn = new string[1024];
-            var count = -1;
-            var lines = File.ReadAllLines(scriptPath);
-                foreach (string lineToDecode in lines)
+            ZipFile.ExtractToDirectory(filePath, dirPath);//Extracts file
+        }
+        public String[] DecodeScriptExecs(string scriptPath)//returns executables to be run
+        {
+            var dataToReturn = new string[1024];//stores data that must be returned
+            var count = -1;//count, -1 cause the first exec will add to it, causing it to increase to 0
+            var lines = File.ReadAllLines(scriptPath);//reads script to memory
+            foreach (string lineToDecode in lines)//reads and processes script, line by line
             {
-                if (lineToDecode.Contains("exec="))
-                {lineToDecode.Replace("exec=", ""); count++; }
-                dataToReturn[count] = lineToDecode;
+                if (lineToDecode.Contains("exec="))//checks for keyword
+                {
+                    lineToDecode.Replace("exec=", ""); //removes keyword, exposing executable path
+                    count++; //adds one to count
+                    dataToReturn[count] = lineToDecode;//adds executable path to variable
+                }
             }
-            return dataToReturn;
+            return dataToReturn;//returns data
         }
     }
 }

c5ef60512d789a1e6f9713e584575c901e82eeda
Daniel Shroff <danielshroff@hotmail.com>
2019-04-09 11:21:24 +0530

    Started work on script parsing

--- src/SCAPP_Launcher_Lib/Launcher.cs
+++ src/SCAPP_Launcher_Lib/Launcher.cs
@@ -1,11 +1,11 @@
 ﻿using System;
-using SharpCompress;
+using System.IO.Compression;
 
 namespace SCAPP_Launcher_Lib
 {
     public class High //High level ops, facade class for easy normal usage, uses Low class
     {
-        public void extractToWorkFolder(string File)
+        public void ExtractToWorkFolder(string File)
         {
             var low = new Low();
             var dirName = File + "-workdir";
@@ -16,6 +16,10 @@ namespace SCAPP_Launcher_Lib
     public class Low //bare metal, use only if required, used by High
     {
         public void ExtractToDir(string filePath, string dirPath)
+        {
+            ZipFile.ExtractToDirectory(filePath, dirPath);
+        }
+        public void DecodeScript(string scriptPath)
         {
 
         }
